3a8b6c0da0268c23e5733b85ab393e1d2c1a8eb8
Alberto <alberto.rcdb@gmail.com>
2018-03-01 08:14:05 +0100

    Añado soporte a la letra ñ mayúscula y minúscula

--- src/Rules/Alpha.php
+++ src/Rules/Alpha.php
@@ -25,7 +25,7 @@ class Alpha implements Rule
      */
     public function passes($attribute, $value)
     {
-        return preg_match('/^[A-Za-záÁéÉíÍóÓúÚüÜ\- ]+$/', $value);
+        return preg_match('/^[A-Za-záÁéÉíÍóÓúÚüÜñÑ\- ]+$/', $value);
     }
 
     /**
